87edfba77653c44e89e7c5eeba310556b9526748
Vitaliy <i@ovitalyo.ru>
2023-09-20 15:57:36 +0300

    commit 1

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,12 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+          <h1>My immortal data</h1>
+        <a href="https://vitejs.dev" target="">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://react.dev" target="">
+              <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
